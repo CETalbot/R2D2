23d3d6379aabb9a26eb0e9eeca322dafc4fc1d06
Catherine Talbot <ctalbot5@uclan.ac.uk>
2017-02-25 15:51:52 +0000

    editing moriarty js file

--- moriartyCanvas.js
+++ moriartyCanvas.js
@@ -1,7 +1,6 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-<<<<<<< HEAD
 //Text in the background
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
@@ -14,12 +13,12 @@ ctx.arc(250,200,40,0,2*Math.PI);
 ctx.stroke();
 
 //the neck
+ctx.beginPath();
 ctx.moveTo(240,265);
 ctx.lineTo(240,240);
 ctx.stroke();
-=======
 
-//Get Sherlock text
+/*Get Sherlock text
 ctx.font = "30px Arial";
-ctx.fillText("GET SHERLOCK", 125, canvas.height/2); 
->>>>>>> d50d967ec3a6b6f7eecf55a58533bcd785399c0b
+ctx.fillText("GET SHERLOCK", 125, canvas.height/2); */
+
