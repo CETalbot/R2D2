7d042d6b1b98dae8c0e43be5ad31563d462bc774
Catherine Talbot <ctalbot5@uclan.ac.uk>
2017-02-25 14:32:42 +0000

    moriarty js file

--- moriartyCanvas.js
+++ moriartyCanvas.js
@@ -1,8 +1,18 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-
+//Text in the background
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
-ctx.fillText("Hello World",10,50);
+ctx.fillText("Get Sherlock",10,50);
+
+//the head
+ctx.beginPath();
+ctx.arc(250,200,40,0,2*Math.PI);
+ctx.stroke();
+
+//the neck
+ctx.moveTo(240,265);
+ctx.lineTo(240,240);
+ctx.stroke();
