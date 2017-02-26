f3f197d5fdb6c77f7096258f03d7d48196096589
Catherine Talbot <ctalbot5@uclan.ac.uk>
2017-02-26 11:22:42 +0000

    editing person

--- moriartyCanvas.js
+++ moriartyCanvas.js
@@ -64,10 +64,50 @@ ctx.bezierCurveTo(318,266,275,340,300,400);
 ctx.stroke();
 //right side of neck
 ctx.beginPath();
-ctx.moveTo(282,225);
+ctx.moveTo(281,225);
 ctx.bezierCurveTo(270,250,265,240,272,260);
 ctx.stroke();
 //arm
+ctx.beginPath();
+ctx.moveTo(295,300);
+ctx.bezierCurveTo(300,310,310,320,350,300);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(293,317);
+ctx.bezierCurveTo(305,335,325,325,350,317);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(295,290);
+ctx.bezierCurveTo(305,305,315,315,350,292);
+ctx.stroke();
+
+//hand
+ctx.beginPath();
+ctx.moveTo(350,300);
+ctx.bezierCurveTo(375,285,380,320,350,317);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(350,292);
+ctx.bezierCurveTo(370,280,380,303,365,315);
+ctx.stroke();
+//eye
+ctx.beginPath();
+ctx.moveTo(285,180);
+ctx.bezierCurveTo(275,180,287,200,290,195);
+ctx.stroke();
+//hair
+ctx.beginPath();
+ctx.moveTo(267,164);
+ctx.bezierCurveTo(215,230,230,195,215,220);
+ctx.stroke();
+//mouth
+ctx.beginPath();
+ctx.moveTo(270,220);
+ctx.bezierCurveTo(270,220,275,230,285,220);
+ctx.stroke();
 /*Get Sherlock text
 ctx.font = "30px Arial";
 ctx.fillText("GET SHERLOCK", 125, canvas.height/2); */
