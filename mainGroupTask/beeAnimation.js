ced00454c4cc432c7727c17d4323574f93b0d9a9
Catherine Talbot <ctalbot5@uclan.ac.uk>
2017-03-15 12:12:47 +0000

    button

--- mainGroupTask/beeAnimation.js
+++ mainGroupTask/beeAnimation.js
@@ -2,9 +2,19 @@ window.onload = function(){
 
 var canvas = document.getElementById("canvasArea");
 var ctx = canvas.getContext("2d");
-function drawingCurve(cp1x,cp1y,cp2x,cp2y,x,y) {
+/*function drawingCurve(cp1x,cp1y,cp2x,cp2y,x,y) {
 ctx.bezierCurveTo(cp1x,cp1y,cp2x,cp2y,x,y);
 ctx.stroke();
 }
-drawingCurve()
+drawingCurve(200,100,165,107,200,120);
+drawingCurve(200,120,230,115,200,100); */
+function drawingCircle(x,y,r) {
+	ctx.beginPath();
+	ctx.arc(x,y,r,0,2*Math.PI);
+	ctx.stroke();
+	ctx.closePath();
+}
+drawingCircle(215,55,40);
+ctx.fillStyle = "yellow";
+ctx.fill();
 }
