98df75701bfc395a8f456c128ed3461a99092957
Natalie Chappelhow <njchappelhow@uclan.ac.uk>
2017-03-15 22:55:33 +0000

    commenting the code

--- mainGroupTask/test.js
+++ mainGroupTask/test.js
@@ -1,5 +1,9 @@
-function PlaySound(path) {
-  var audioElement = document.createElement('audio');
-  audioElement.setAttribute('src', 'Bumble-bee-sound.mp3');
-  audioElement.play();
-}
+
+
+
+//Used https://siongui.github.io/2012/10/08/javascript-play-sound-onclick/ and https://www.w3schools.com/jsref/met_document_createelement.asp to get the sound playing by clicking on the button
+function playBeeBuzzing() { //creating a function called 'playBeeBuzzing'. This function does not have any parameters declared. It is undefined
+  var addingAudio = document.createElement('audio');//creating an audio element. We found that the sound would only work when the button has been pressed once we had added audio as an element
+  addingAudio.setAttribute('src', 'Bumble-bee-sound.mp3');// used https://www.w3schools.com/jsref/met_element_setattribute.asp to understand how to setAttribute. This line is stating the path for the audio file
+  addingAudio.play();//playing the audio element
+};
