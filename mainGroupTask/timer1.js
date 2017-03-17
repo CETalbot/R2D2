fa890ee51f78c069f5e16882e3fbf99207fdbaa0
Bhakti <bypatel@uclan.ac.uk>
2017-03-17 13:14:13 +0000

    pushing comments

--- mainGroupTask/timer1.js
+++ mainGroupTask/timer1.js
@@ -1,7 +1,7 @@
-var c = 0;
+var c = 0; //variable is used. the counting will start from number 0
 
 function timer() {
-    setInterval("count()", 1000);
+    setInterval("count()", 1000); // function set interval is used to count the number. it will count till the number 1000
 }
 
 function count() {
