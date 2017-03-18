7ec3505a40842dbc30ff99e991817530c9ea431e
Natalie Chappelhow <njchappelhow@uclan.ac.uk>
2017-03-18 15:17:16 +0000

    adding colour to flower pot

--- mainGroupTask/beeAnimation.js
+++ mainGroupTask/beeAnimation.js
@@ -26,27 +26,27 @@ function drawCurve(a,b,c,d,e,f,x,y){
     ctx.beginPath();//beginning the path to draw the curve
     ctx.moveTo(a,b);//start point of the bezier curve
     ctx.bezierCurveTo(c,d,e,f,x,y);//the first four numbers are the control points of the bezier curve (the first two numbers are for one control point and the last two numbers of the four are for the second control point). The last two numbers are for the end point of the bezier curve    
-    ctx.closePath();//closing the paths
     ctx.stroke();//drawing the curves
-    ctx.strokeStyle = "brown"; //choosing the colour brown for the stroke stule
-    ctx.fill();//colouring in the paths
-    ctx.fillStyle = "brown"; //choosing the colour brown for the fill
 };
-	drawCurve(150,550,200,555,190,555,250,550);//bottom curve of the plantpot
-	drawCurve(120,460,200,465,190,465,280,460);//top curve of the plantpot
-
-
-
+	
+   
 function drawLine (a,b,c,d){//creating a function called 'drawLine'. As I would like to be able to change the co-ordinates for each drawn line I have given the function the parameters of a,b,c and d
     ctx.moveTo(a,b);//beginning the drawing of the line by saying what co-ordinates the line will start from
     ctx.lineTo(c,d);//ending the drawing of the line by saying what co-ordinates the line will end at
-    ctx.closePath();//closing the paths
     ctx.stroke();//drawing the line
-    ctx.strokeStyle = "brown"; //choosing the colour brown for the stroke stule
-    ctx.fill();//colouring in the paths
-    ctx.fillStyle = "brown"; //choosing the colour brown for the fill
+
 };
+
+	ctx.beginPath();
+
+	drawCurve(150,550,200,555,190,555,250,550);//bottom curve of the plantpot
+	drawCurve(120,460,200,465,190,465,280,460);//top curve of the plantpot
+
 	drawLine(120,460,150,550);//left hand side of the plant pot
 	drawLine(280,460,250,550);// right hand side of the plant pot
 
+	ctx.closePath();//closing the paths
+    ctx.fillStyle = "brown"; //choosing the colour brown for the fill
+    ctx.fill();//colouring in the paths
+    ctx.strokeStyle = "brown"; //choosing the colour brown for the stroke style
 }
--- wall-eTask2/walleCanvas.js
+++ wall-eTask2/walleCanvas.js
@@ -7,6 +7,49 @@ var ctx = canvas.getContext("2d");
 function rectangleDraw(x,y,w,h){
 	ctx.rect(x,y,w,h);		
 	ctx.stroke();
-}
-	rectangleDraw(180,230,400,300);
-}
+};
+	rectangleDraw(180,230,300,300);
+	//rectangleDraw(200,370,30,40);//left side top of arm
+	//rectangleDraw(530,370,30,40);//right side top of arm
+
+
+//drawing arms
+function bezierCurveDraw(a,b,c,d,e,f,x,y){
+	ctx.beginPath();//beginning the path to draw the curve
+    ctx.moveTo(a,b);//start point of the bezier curve
+    ctx.bezierCurveTo(c,d,e,f,x,y);//the first four numbers are the control points of the bezier curve (the first two numbers are for one control point and the last two numbers of the four are for the second control point). The last two numbers are for the end point of the bezier curve    
+    ctx.closePath();//closing the paths
+    ctx.stroke();//drawing the curves
+};
+	bezierCurveDraw(173,423,100,100,100,100,203,423);
+
+function lineDraw(a,b,c,d){
+	ctx.moveTo(a,b);//beginning the drawing of the line by saying what co-ordinates the line will start from
+	ctx.lineTo(c,d);//ending the drawing of the line by saying what co-ordinates the line will end at
+	ctx.stroke();//drawing the lines
+};
+	//lineDraw(160,410,180,410);//left side body to arm conection
+	//lineDraw(580,410,600,410);//right side body to arm conection
+
+	lineDraw(213,373,173,373);//left arm top line connecting hand to arm
+	lineDraw(173,373,173,423);//left arm left line connecting hand to arm
+	lineDraw(203,400,203,423);//left arm right line connecting hand to arm
+
+
+//drawing hands
+
+//top claw left hand
+	lineDraw(230,330,200,400);//back line of left hand
+	lineDraw(230,363,226,373);//second back line of the left side at the back
+	lineDraw(230,330,276,340);//top tine of left hand back line
+	lineDraw(220,360,265,372);//second line of left hand back line
+	lineDraw(276,340,265,372);//closing line of the left side at the back
+
+//bottom claw left hand
+	
+	lineDraw(212,370,259,382);//third line of left hand back line
+	lineDraw(245,414,200,400);//fourth line of left hand
+	lineDraw(258,383,245,415);//closing line of the left side at the back
+};
+
+
