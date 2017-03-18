b8622afbfeee00220aa2b8c5a97b90563107c982
Natalie Chappelhow <njchappelhow@uclan.ac.uk>
2017-03-18 13:00:21 +0000

    drawing flower pot

--- mainGroupTask/beeAnimation.js
+++ mainGroupTask/beeAnimation.js
@@ -2,12 +2,14 @@ window.onload = function(){
 
 var canvas = document.getElementById("canvasArea");
 var ctx = canvas.getContext("2d");
+
+
 /*function drawingCurve(cp1x,cp1y,cp2x,cp2y,x,y) {
 ctx.bezierCurveTo(cp1x,cp1y,cp2x,cp2y,x,y);
 ctx.stroke();
 }
 drawingCurve(200,100,165,107,200,120);
-drawingCurve(200,120,230,115,200,100); */
+drawingCurve(200,120,230,115,200,100); 
 function drawingCircle(x,y,r) {
 	ctx.beginPath();
 	ctx.arc(x,y,r,0,2*Math.PI);
@@ -16,37 +18,35 @@ function drawingCircle(x,y,r) {
 }
 drawingCircle(215,55,40);
 ctx.fillStyle = "yellow";
-ctx.fill();
+ctx.fill();*/
 
 
-/*var audio = new Audio("Bumble-bee-sound.mp3");
-audio.oncanplaythrough = function(){
-	audio.play();
-}
-
-audio.loop = false;
-
-audio.onended = function(){
-	audio.play();
-}*/
-
-
-
-/*document.getElementById("demo").onclick = function() {myFunction()};
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Bumble-bee-sound.mp3";
-}*/
-
-
-
-//Used https://siongui.github.io/2012/10/08/javascript-play-sound-onclick/ and https://www.w3schools.com/jsref/met_document_createelement.asp to get the sound playing by clicking on the button
-function playBeeBuzzing() { //creating a function called 'playBeeBuzzing'. This function does not have any parameters declared. It is undefined
-  var addingAudio = document.createElement('audio');//creating an audio element. We found that the sound would only work when the button has been pressed once we had added audio as an element
-  addingAudio.setAttribute('src', 'Bumble-bee-sound.mp3');// used https://www.w3schools.com/jsref/met_element_setattribute.asp to understand how to setAttribute. This line is stating the path for the audio file
-  addingAudio.play();//playing the audio element
+//drawing the plant pot
+function drawCurve(a,b,c,d,e,f,x,y){
+    ctx.beginPath();//beginning the path to draw the curve
+    ctx.moveTo(a,b);//start point of the bezier curve
+    ctx.bezierCurveTo(c,d,e,f,x,y);//the first four numbers are the control points of the bezier curve (the first two numbers are for one control point and the last two numbers of the four are for the second control point). The last two numbers are for the end point of the bezier curve    
+    ctx.closePath();//closing the paths
+    ctx.stroke();//drawing the curves
+    ctx.strokeStyle = "brown"; //choosing the colour brown for the stroke stule
+    ctx.fill();//colouring in the paths
+    ctx.fillStyle = "brown"; //choosing the colour brown for the fill
 };
+	drawCurve(150,550,200,555,190,555,250,550);//bottom curve of the plantpot
+	drawCurve(120,460,200,465,190,465,280,460);//top curve of the plantpot
 
 
+
+function drawLine (a,b,c,d){//creating a function called 'drawLine'. As I would like to be able to change the co-ordinates for each drawn line I have given the function the parameters of a,b,c and d
+    ctx.moveTo(a,b);//beginning the drawing of the line by saying what co-ordinates the line will start from
+    ctx.lineTo(c,d);//ending the drawing of the line by saying what co-ordinates the line will end at
+    ctx.closePath();//closing the paths
+    ctx.stroke();//drawing the line
+    ctx.strokeStyle = "brown"; //choosing the colour brown for the stroke stule
+    ctx.fill();//colouring in the paths
+    ctx.fillStyle = "brown"; //choosing the colour brown for the fill
+};
+	drawLine(120,460,150,550);//left hand side of the plant pot
+	drawLine(280,460,250,550);// right hand side of the plant pot
 
 }
