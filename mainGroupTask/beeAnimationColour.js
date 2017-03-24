6bf4b29d140c807d1bddc78247f80630e9b40e57
Natalie Chappelhow <njchappelhow@uclan.ac.uk>
2017-03-24 15:40:59 +0000

    completed bee

--- mainGroupTask/beeAnimationColour.js
+++ mainGroupTask/beeAnimationColour.js
@@ -7,7 +7,7 @@ function drawingCircle(x,y,r) {
     
 	ctx.arc(x,y,r,0,2*Math.PI);
 	ctx.stroke();
-}
+};
 
 //drawing the plant pot
 function drawingCurve(c,d,e,f,x,y){
@@ -15,6 +15,11 @@ function drawingCurve(c,d,e,f,x,y){
     ctx.bezierCurveTo(c,d,e,f,x,y);//the first four numbers are the control points of the bezier curve (the first two numbers are for one control point and the last two numbers of the four are for the second control point). The last two numbers are for the end point of the bezier curve    
     ctx.stroke();//drawing the curves
 };
+
+
+
+    
+
 
 function drawingLine (c,d){//creating a function called 'drawLine'. As I would like to be able to change the co-ordinates for each drawn line I have given the function the parameters of a,b,c and d
     ctx.lineTo(c,d);//ending the drawing of the line by saying what co-ordinates the line will end at
@@ -37,11 +42,11 @@ function drawingRectangle (x,y,w,h){
     ctx.fill();//colouring in the paths
     
 
+ 
+
 
 //drawing bee
-
-
-    ctx.beginPath();
+    ctx.beginPath();//body of the bee
     ctx.save();
     ctx.scale(2,1);
     drawingCircle(100,150,40);
@@ -53,22 +58,15 @@ function drawingRectangle (x,y,w,h){
     ctx.fill();
 
 
-    ctx.beginPath();
-    drawingRectangle(130,130,10,40);
-    drawingRectangle(160,115,10,70);
-    drawingRectangle(190,110,10,80);
-    drawingRectangle(220,110,10,80);
-    ctx.closePath();
-    ctx.fillStyle = "black";
-    ctx.fill();
-
-    ctx.beginPath();
+    ctx.beginPath();//eye of the bee
     drawingCircle(260,143,6);
     ctx.closePath();
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.fillStyle = "black";
     ctx.fill();
+
+
 /*sky background colour gradient code used from 
 https://www.w3schools.com/tags/canvas_createlineargradient.asp on 05/03/2017*/
     var gradient=ctx.createRadialGradient(170,100,2,200,30,120);
@@ -81,4 +79,36 @@ https://www.w3schools.com/tags/canvas_createlineargradient.asp on 05/03/2017*/
     ctx.fillStyle = gradient;
     ctx.fill();
     ctx.closePath();
+
+//behind stripe of the bee
+    ctx.beginPath();
+    drawingCurve(142,123,90,140,141,178);
+    ctx.moveTo(142,123);
+    drawingLine(142,178);
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+//second stripe of the bee
+    ctx.beginPath();
+    drawingCurve(160,115,150,150,160,185);//left line
+    drawingCurve(158,185,160,185,178,189);//bottom line
+    drawingCurve(172,135,172,170,178,111);//right line
+    drawingCurve(178,111,165,115,160,115)//top line 
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+//first stripe of the bee
+    ctx.beginPath();
+    drawingCurve(200,110,185,150,200,190);//left line
+    drawingCurve(202,185,190,192,220,189);//bottom line
+    drawingCurve(221,189,200,150,220,110);//right line
+    drawingCurve(220,110,190,113,201,110);//top line
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ctx.beginPath();
+    drawingCurve(240,160,240,180,270,170);//smiley face
 }
--- wall-eTask2/walleCanvas.js
+++ wall-eTask2/walleCanvas.js
@@ -17,10 +17,8 @@ function rectangleDraw(x,y,w,h){
 };
 	rectangleDraw(180,230,300,300);//main body shape
 
-	//rectangleDraw(200,370,30,40);//left side top of arm
-	//rectangleDraw(530,370,30,40);//right side top of arm
 
-//drawing leggs
+//drawing legs
 function drawRectangle(x,y,w,h){
 	ctx.rect(x,y,w,h);		
 	ctx.stroke();
@@ -32,89 +30,10 @@ function drawRectangle(x,y,w,h){
     drawRectangle(290,610,10,10);//fourth rectangle in left leg
 
     drawRectangle(350,530,110,80);//right leg
-    drawRectangle(360, 610,10,10);//first rectangle in right leg
-    drawRectangle(380, 610,20,10);//second rectangle in right leg
-    drawRectangle(410, 610,20,10);//third rectangle in right leg
-    drawRectangle(440, 610,10,10);//fourth rectangle in right leg
-
-
-function drawLine(a,b,c,d){
-	ctx.moveTo(a,b);
-	ctx.lineTo(c,d);
-	ctx.stroke();
-};
-    drawLine(180,530,200,550);//line connecting to left leg from the body
-    drawLine(480,530,460,550);//line connecting to right leg from the body
-    drawLine(310,560,333,560);//straight line between two legs
-    drawLine(333,560,350,545);//diagonal line between two legs
-    
-    //////////left leg/////////
-    //first horizontal in left leg
-    drawLine(230,545,210,555);//first line in left leg
-    drawLine(230,550,210,560);//second line in left leg
-    drawLine(230,565,210,575);//third line in left leg
-    drawLine(230,570,210,580);//fourth line in left leg 
-    drawLine(230,585,210,595);//fifth line in left leg
-    drawLine(230,590,210,600);//sixth line in left leg
-
-   //second horizontal in left leg
-   drawLine(230,545,250,555);//first line in left leg
-   drawLine(230,550,250,560);//second line in left leg
-   drawLine(230,565,250,575);//third line in left leg
-   drawLine(230,570,250,580);//fourth line in left leg
-   drawLine(230,585,250,595);//fifth line in left leg
-   drawLine(230,590,250,600);//sixth line in left leg
-
-   //third horizontal in left leg
-   drawLine(280,545,260,555);//first line in left leg
-   drawLine(280,550,260,560);//second line in left leg
-   drawLine(280,565,260,575);//third line in left leg
-   drawLine(280,570,260,580);//fourth line in left leg
-   drawLine(280,585,260,595);//fifth line in left leg
-   drawLine(280,590,260,600);//sixth line in left leg
- 
-   //fourth horizontal in left leg
-   drawLine(280,545,300,555);//first line in left leg
-   drawLine(280,550,300,560);//second line in left leg
-   drawLine(280,565,300,575);//third line in left leg
-   drawLine(280,570,300,580);//fourth line in left leg
-   drawLine(280,585,300,595);//fifth line in left leg
-   drawLine(280,590,300,600);//sixth line in left leg
-
-   //////////right leg////////
-   //first horizontal in right leg
-   drawLine(380,545,360,555);//first line in left leg
-   drawLine(380,550,360,560);//second line in left leg
-   drawLine(380,565,360,575);//third line in left leg
-   drawLine(380,570,360,580);//fourth line in left leg 
-   drawLine(380,585,360,595);//fifth line in left leg
-   drawLine(380,590,360,600);//sixth line in left leg
-
-   //second horizontal in left leg
-   drawLine(380,545,400,555);//first line in left leg
-   drawLine(380,550,400,560);//second line in left leg
-   drawLine(380,565,400,575);//third line in left leg
-   drawLine(380,570,400,580);//fourth line in left leg 
-   drawLine(380,585,400,595);//fifth line in left leg
-   drawLine(380,590,400,600);//sixth line in left leg
-
-   //third horizontal in left leg
-   drawLine(430,545,410,555);//first line in left leg
-   drawLine(430,550,410,560);//second line in left leg
-   drawLine(430,565,410,575);//third line in left leg
-   drawLine(430,570,410,580);//fourth line in left leg
-   drawLine(430,585,410,595);//fifth line in left leg
-   drawLine(430,590,410,600);//sixth line in left leg
- 
-   //fourth horizontal in left leg
-   drawLine(430,545,450,555);//first line in left leg
-   drawLine(430,550,450,560);//second line in left leg
-   drawLine(430,565,450,575);//third line in left leg
-   drawLine(430,570,450,580);//fourth line in left leg
-   drawLine(430,585,450,595);//fifth line in left leg
-   drawLine(430,590,450,600);//sixth line in left leg
-
-
+    drawRectangle(360,610,10,10);//first rectangle in right leg
+    drawRectangle(380,610,20,10);//second rectangle in right leg
+    drawRectangle(410,610,20,10);//third rectangle in right leg
+    drawRectangle(440,610,10,10);//fourth rectangle in right leg
 
 
 
@@ -129,46 +48,153 @@ function bezierCurveDraw(a,b,c,d,e,f,x,y){
 };
 	bezierCurveDraw(173,423,200,430,180,430,203,423);//left side 
 
-function lineDraw(a,b,c,d){
+function drawLine(a,b,c,d){
 	ctx.moveTo(a,b);//beginning the drawing of the line by saying what co-ordinates the line will start from
 	ctx.lineTo(c,d);//ending the drawing of the line by saying what co-ordinates the line will end at
 	ctx.stroke();//drawing the lines
 };
 	
-
-	lineDraw(213,373,173,373);//left arm top line connecting hand to arm
-	lineDraw(173,373,173,423);//left arm left line connecting hand to arm
-	lineDraw(203,400,203,423);//left arm right line connecting hand to arm
-	lineDraw(173,373,103,393);//top line left arm
-	lineDraw(173,423,103,443);//bottom line left arm
-	lineDraw(103,393,103,443);//closing left arm
+  //drawing arms
+  //left arm
+	drawLine(213,373,173,373);//left arm top line connecting hand to arm
+	drawLine(173,373,173,423);//left arm left line connecting hand to arm
+	drawLine(203,400,203,423);//left arm right line connecting hand to arm
+	drawLine(173,373,103,393);//top line left arm
+	drawLine(173,423,103,443);//bottom line left arm
+	drawLine(103,393,103,443);//closing left arm
+  drawLine(153,430,180,430);//line underneath arm
 
 //drawing hands
-
 //top claw left hand - back
-	lineDraw(230,330,200,400);//back line of left hand
-	lineDraw(230,363,226,373);//second back line of the left side at the back
-	lineDraw(230,330,276,340);//top line of left hand back line
-	lineDraw(220,360,265,372);//second line of left hand back line
-	lineDraw(276,340,265,372);//closing line of the left side at the back
+	drawLine(230,330,200,400);//back line of left hand
+	drawLine(230,363,226,373);//second back line of the left side at the back
+	drawLine(230,330,276,340);//top line of left hand back line
+	drawLine(220,360,265,372);//second line of left hand back line
+	drawLine(276,340,265,372);//closing line of the left side at the back
 
 //bottom claw left hand - back
-	
-	lineDraw(212,370,259,382);//third line of left hand back line
-	lineDraw(245,414,200,400);//fourth line of left hand
-	lineDraw(258,383,245,415);//closing line of the left side at the back
-
-
+	drawLine(212,370,259,382);//third line of left hand back line
+	drawLine(245,414,200,400);//fourth line of left hand
+	drawLine(258,383,245,415);//closing line of the left side at the back
 
 //top claw left hand - front
-	lineDraw(276,340,319,360);//top line of left hand back line
-	lineDraw(265,372,306,392);//second line of left hand back line
-	lineDraw(306,392,319,359);//closing line of the left side at the front
+	drawLine(276,340,319,360);//top line of left hand back line
+	drawLine(265,372,306,392);//second line of left hand back line
+	drawLine(306,392,319,359);//closing line of the left side at the front
 
 //bottom claw left hand - front
-	lineDraw(259,382,299,402);//third line of left hand back line
-	lineDraw(245,414,285,434);//fourth line of left hand
-	lineDraw(285,434,300,401);//closing line of the left side at the front
+	drawLine(259,382,299,402);//third line of left hand back line
+	drawLine(245,414,285,434);//fourth line of left hand
+	drawLine(285,434,300,401);//closing line of the left side at the front
+
+
+//right arm
+  drawLine(213,373,173,373);//right arm top line connecting hand to arm
+  drawLine(173,373,173,423);//right arm left line connecting hand to arm
+  drawLine(203,400,203,423);//right arm right line connecting hand to arm
+  drawLine(173,373,103,393);//top line right arm
+  drawLine(173,423,103,443);//bottom line right arm
+  drawLine(103,393,103,443);//closing right arm
+  drawLine(480,430,507,430);//line underneath arm
+
+//drawing hands
+//top claw right hand - back
+  drawLine(230,330,200,400);//back line of right hand
+  drawLine(230,363,226,373);//second back line of the right side at the back
+  drawLine(230,330,276,340);//top line of right hand back line
+  drawLine(220,360,265,372);//second line of right hand back line
+  drawLine(276,340,265,372);//closing line of the right side at the back
+
+//bottom claw right hand - back
+  drawLine(212,370,259,382);//third line of right hand back line
+  drawLine(245,414,200,400);//fourth line of right hand
+  drawLine(258,383,245,415);//closing line of the right side at the back
+
+//top claw right hand - front
+  drawLine(276,340,319,360);//top line of right hand back line
+  drawLine(265,372,306,392);//second line of right hand back line
+  drawLine(306,392,319,359);//closing line of the right side at the front
+
+//bottom claw right hand - front
+  drawLine(259,382,299,402);//third line of right hand back line
+  drawLine(245,414,285,434);//fourth line of right hand
+  drawLine(285,434,300,401);//closing line of the right side at the front
+
+
+
+
+
+//drawing legs
+  drawLine(180,530,200,550);//line connecting to left leg from the body
+  drawLine(480,530,460,550);//line connecting to right leg from the body
+  drawLine(310,560,333,560);//straight line between two legs
+  drawLine(333,560,350,545);//diagonal line between two legs
+    
+    //////////left leg/////////
+    //first horizontal in left leg
+  drawLine(230,545,210,555);//first line in left leg
+  drawLine(230,550,210,560);//second line in left leg
+  drawLine(230,565,210,575);//third line in left leg
+  drawLine(230,570,210,580);//fourth line in left leg 
+  drawLine(230,585,210,595);//fifth line in left leg
+  drawLine(230,590,210,600);//sixth line in left leg
+
+   //second horizontal in left leg
+  drawLine(230,545,250,555);//first line in left leg
+  drawLine(230,550,250,560);//second line in left leg
+  drawLine(230,565,250,575);//third line in left leg
+  drawLine(230,570,250,580);//fourth line in left leg
+  drawLine(230,585,250,595);//fifth line in left leg
+  drawLine(230,590,250,600);//sixth line in left leg
+   //third horizontal in left leg
+  drawLine(280,545,260,555);//first line in left leg
+  drawLine(280,550,260,560);//second line in left leg
+  drawLine(280,565,260,575);//third line in left leg
+  drawLine(280,570,260,580);//fourth line in left leg
+  drawLine(280,585,260,595);//fifth line in left leg
+  drawLine(280,590,260,600);//sixth line in left leg
+ 
+   //fourth horizontal in left leg
+  drawLine(280,545,300,555);//first line in left leg
+  drawLine(280,550,300,560);//second line in left leg
+  drawLine(280,565,300,575);//third line in left leg
+  drawLine(280,570,300,580);//fourth line in left leg
+  drawLine(280,585,300,595);//fifth line in left leg
+  drawLine(280,590,300,600);//sixth line in left leg
+   //////////right leg////////
+   //first horizontal in right leg
+  drawLine(380,545,360,555);//first line in left leg
+  drawLine(380,550,360,560);//second line in left leg
+  drawLine(380,565,360,575);//third line in left leg
+  drawLine(380,570,360,580);//fourth line in left leg 
+  drawLine(380,585,360,595);//fifth line in left leg
+  drawLine(380,590,360,600);//sixth line in left leg
+   //second horizontal in left leg
+  drawLine(380,545,400,555);//first line in left leg
+  drawLine(380,550,400,560);//second line in left leg
+  drawLine(380,565,400,575);//third line in left leg
+  drawLine(380,570,400,580);//fourth line in left leg 
+  drawLine(380,585,400,595);//fifth line in left leg
+  drawLine(380,590,400,600);//sixth line in left leg
+
+   //third horizontal in left leg
+  drawLine(430,545,410,555);//first line in left leg
+  drawLine(430,550,410,560);//second line in left leg
+  drawLine(430,565,410,575);//third line in left leg
+  drawLine(430,570,410,580);//fourth line in left leg
+  drawLine(430,585,410,595);//fifth line in left leg
+  drawLine(430,590,410,600);//sixth line in left leg
+    //fourth horizontal in left leg
+  drawLine(430,545,450,555);//first line in left leg
+  drawLine(430,550,450,560);//second line in left leg
+  drawLine(430,565,450,575);//third line in left leg
+  drawLine(430,570,450,580);//fourth line in left leg
+  drawLine(430,585,450,595);//fifth line in left leg
+  drawLine(430,590,450,600);//sixth line in left leg
+
+
+
+
 
 drawingCircle(440,500,22);
 ctx.fillStyle = "red";
