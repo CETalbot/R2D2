cba7e1c66c6e9647bd11ebd6b82645f935627fe0
lmiddleton <lmiddleton2@uclan.ac.uk>
2017-04-07 14:34:46 +0100

    fixed increment bee onclick

---
+++ test.js
@@ -0,0 +1,20 @@
+var total =0;
+var canvas = document.getElementById("canvasArea");
+var ctx = canvas.getContext("2d");
+// increment
+
+
+function BtnClick() {
+  total++;
+  document.getElementById('inc').value =total;
+
+}
+
+//bee image
+
+function Bee(){
+  document.getElementById('pic').src="bee.svg";
+}
+document.getElementById('beeButton').onclick = function() {
+
+}
