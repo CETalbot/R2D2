65f0fe15af390a557a88073f427046fd7b03284f
lmiddleton <lmiddleton2@uclan.ac.uk>
2017-05-08 14:32:03 +0100

    updated comments and changed click count to sessionButtonClicked

--- mainGroupTask/beeFlowerPaths/localStorage.js
+++ mainGroupTask/beeFlowerPaths/localStorage.js
@@ -10,24 +10,24 @@ document.getElementById("buttonReset").addEventListener("click", reset);// get t
 
 function Session1() { // create function session 1
     if(typeof(Storage) !== "undefined") { // if local storage is supported in the browser.
-        if (localStorage.clickcount1) { // if the session one button is clicked
-            localStorage.clickcount1 = Number(localStorage.clickcount1)+1; // store the button click (clickcount1) in the local storage and add one to the current value
+        if (localStorage.session1ButtonClicked) { // if the session one button is clicked
+            localStorage.session1ButtonClicked = Number(localStorage.session1ButtonClicked)+1; // store the button click  in the local storage and add one to the current value
         } else { // if button isint pressed
-            localStorage.clickcount1 = 1; // show the current value that is stored inside the local storage
+            localStorage.session1ButtonClicked = 1; // show the current value that is stored inside the local storage
         }
-        document.getElementById("result1").innerHTML = + localStorage.clickcount1 // get the id result1 and display the current value of the session one button press (clickcount1) on the html page.
+        document.getElementById("session1CurrentValue").innerHTML = + localStorage.session1ButtonClicked // get the id session1CurrentValue and display the current value of the session one button press  on the html page.
     }
 }
 
 
 function Session2() { // create function session 2
     if(typeof(Storage) !== "undefined") {  // if local storage is supported in the browser.
-        if (localStorage.clickcount2) { // if the session two button is clicked
-            localStorage.clickcount2 = Number(localStorage.clickcount2)+1; // store the button click (clickcount2) in the local storage and add one to the current value
+        if (localStorage.session2ButtonClicked) { // if the session two button is clicked
+            localStorage.session2ButtonClicked = Number(localStorage.session2ButtonClicked)+1; // store the button click  in the local storage and add one to the current value
         } else { // if button isint pressed
-            localStorage.clickcount2 = 1; // show the current value that is stored inside the local storage
+            localStorage.session2ButtonClicked = 1; // show the current value that is stored inside the local storage
         }
-        document.getElementById("result2").innerHTML = + localStorage.clickcount2 // get the id result2 and display the current value of the session two button press (clickcount2) on the html page.
+        document.getElementById("session2CurrentValue").innerHTML = + localStorage.session2ButtonClicked // get the id session2CurrentValue and display the current value of the session two button press on the html page.
     }
     }
 
@@ -35,12 +35,12 @@ function Session2() { // create function session 2
 
 function Session3() { // create function session 3
     if(typeof(Storage) !== "undefined") { // if local storage is supported in the browser.
-        if (localStorage.clickcount3) { // if the session three button is clicked
-            localStorage.clickcount3 = Number(localStorage.clickcount3)+1; // store the button click (clickcount3) in the local storage and add one to the current value
+        if (localStorage.session3ButtonClicked) { // if the session three button is clicked
+            localStorage.session3ButtonClicked = Number(localStorage.session3ButtonClicked)+1; // store the button click  in the local storage and add one to the current value
         } else { // if button isint pressed
-            localStorage.clickcount3 = 1; // show the current value that is stored inside the local storage
+            localStorage.session3ButtonClicked = 1; // show the current value that is stored inside the local storage
         }
-        document.getElementById("result3").innerHTML = + localStorage.clickcount3 // get the id result3 and display the current value of the session three button press (clickcount3) on the html page.
+        document.getElementById("session3CurrentValue").innerHTML = + localStorage.session3ButtonClicked // get the id session3CurrentValue and display the current value of the session three button press  on the html page.
     }
     }
 
@@ -48,12 +48,12 @@ function Session3() { // create function session 3
 
 function Session4() { // create function session 4
     if(typeof(Storage) !== "undefined") { // if local storage is supported in the browser.
-        if (localStorage.clickcount4) { //  if the session four button is clicked
-            localStorage.clickcount4 = Number(localStorage.clickcount4)+1; // store the button click (clickcount4) in the local storage and add one to the current value
+        if (localStorage.session4ButtonClicked) { //  if the session four button is clicked
+            localStorage.session4ButtonClicked = Number(localStorage.session4ButtonClicked)+1; // store the button click  in the local storage and add one to the current value
         } else { // if button isint pressed
-            localStorage.clickcount4 = 1; // show the current value that is stored inside the local storage
+            localStorage.session4ButtonClicked = 1; // show the current value that is stored inside the local storage
         }
-        document.getElementById("result4").innerHTML = + localStorage.clickcount4 // get the id result4 and display the current value of the session four button press (clickcount4) on the html page.
+        document.getElementById("session4CurrentValue").innerHTML = + localStorage.session4ButtonClicked // get the id session4CurrentValue and display the current value of the session four button press  on the html page.
     }
     }
 
@@ -61,12 +61,12 @@ function Session4() { // create function session 4
 
 function Session5() { // create function session 5
     if(typeof(Storage) !== "undefined") {  // if local storage is supported in the browser.
-        if (localStorage.clickcount5) { //  if the session five button is clicked
-            localStorage.clickcount5 = Number(localStorage.clickcount5)+1;  // store the button click (clickcount5) in the local storage and add one to the current value
+        if (localStorage.session5ButtonClicked) { //  if the session five button is clicked
+            localStorage.session5ButtonClicked = Number(localStorage.session5ButtonClicked)+1;  // store the button click in the local storage and add one to the current value
         } else { // if button isint pressed
-            localStorage.clickcount5 = 1; // show the current value that is stored inside the local storage
+            localStorage.session5ButtonClicked = 1; // show the current value that is stored inside the local storage
         }
-        document.getElementById("result5").innerHTML = + localStorage.clickcount5 // get the id result5 and display the current value of the session five button press (clickcount5) on the html page.
+        document.getElementById("session5CurrentValue").innerHTML = + localStorage.session5ButtonClicked // get the id session5CurrentValue and display the current value of the session five button press on the html page.
     }
     }
 
